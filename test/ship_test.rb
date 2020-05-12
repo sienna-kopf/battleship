125b6c407907da7a6719622ba282b1db7b951bbd
Sienna Kopf <princess.kopf@gmail.com>
2020-05-11 20:53:08 -0600

    Add ship test if it exists, has a name, and has a length

---
+++ test/ship_test.rb
@@ -0,0 +1,25 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ship'
+
+class ShipTest < Minitest::Test
+  def test_it_exists
+    cruiser = Ship.new("Cruiser", 3)
+
+    assert_instance_of Ship, cruiser
+  end
+
+  def test_it_has_a_name
+    cruiser = Ship.new("Cruiser", 3)
+
+    assert_equal "Cruiser", cruiser.name
+  end
+
+  def test_it_has_a_length
+    cruiser = Ship.new("Cruiser", 3)
+
+    assert_equal 3, cruiser.length
+  end
+
+
+end
