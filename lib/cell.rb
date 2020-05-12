45b7ec7ccef0f58d452e294e9e5b91c02ad84deb
Sienna Kopf <princess.kopf@gmail.com>
2020-05-12 11:52:37 -0600

    Add cell class working but questionable solution

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,8 +1,24 @@
 class Cell
-  attr_reader :coordinate
+  attr_reader :coordinate,
+              :ship_obj
 
   def initialize(coordinate)
     @coordinate = coordinate
+    # @empty = false
+    @ship_obj = ship_obj
+  end
+
+  def empty?
+    return false if ship != nil
+    true
+  end
+
+  def place_ship(name)
+    @ship_obj = name
+  end
+
+  def ship
+    @ship_obj
   end
 
 end
