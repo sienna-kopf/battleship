4c4fbe0446551f66efc62194dfc5253d4ecca510
Sienna Kopf <princess.kopf@gmail.com>
2020-05-15 15:50:03 -0600

    Add board functionality for placing ships

--- lib/board.rb
+++ lib/board.rb
@@ -37,7 +37,7 @@ class Board
         if coordinates[1].to_i + 1 == coordinates[3].to_i
           return true
         else
-          return false 
+          return false
         end
       elsif coordinates[0].ord + 1 == coordinates[2].ord
         if coordinates[1].to_i == coordinates[3].to_i
@@ -65,69 +65,20 @@ class Board
         return false
       end
     end
-  end
-    # end
 
     #     # think about splitting the array into numbers and letters
     #     # iterate through odd ele (LETTERS) and even ele (NUMBERS)
     #     # break down to simplist possible version
     #     # simplist way to compare coordinates is compare a coordinate to itself. Do I want to split it, take ord values etc
     #     # second most complicated is thinking about two coordinates. Not necessarily iterating
-    #
-    # else
-    #   coordinates.each do |coord|  ## not necessarily need to iterate.
-    #
-    #   end
-    # end
 
-  #   coordinates.each_cons(2) do |coord|
-  #     #rather than each_cons... maybe start with each and then figure out what enumerable allows you to do what you want to do
-  #       if coord[0][0].ord == coord[1][0].ord
-  #         if coord[0][1].to_i + 1 == coord[1][1].to_i
-  #           valid_collector << :valid_coordinate
-  #         end
-  #       elsif coord[0][0].ord + 1 == coord[1][0].ord
-  #         if coord[0][1].to_i == coord[1][1].to_i
-  #           valid_collector << :valid_coordinate
-  #         end
-  #       else
-  #         require "pry"; binding.pry
-  #         valid_collector << :invalid_coordinate
-  #       end
-  #   end
-  #
-  # end
-  #
-  # valid_collector.all? do |verification|
-  #   if verification == :valid_coordinate
-  #     true
-  #   end
-  # end
+  end
 
+  def place(ship, coordinates)
+    coordinates.each do |coordinate|
+      @cells[coordinate].place_ship(ship)
+    end
 
+  end
 
-
-    # this method will be long at the end, think about refactoring accoridng to SRP. GET THROUGH PROJECT FIRST
-
-    #look on project page for other methods
-
-    #each_cons method for hashes
-    #take each consecutive pair in block have first and second ele and then next time it increments
-    # key doesnt have to be an integer
-
-    # .ord ordinal value
-    #could be helpful for compare, works for integers and alphebet
-
-  #   index = 0
-  #   (ship.length == coordinates.count &&
-  #    coordinates[index][1].to_i + 1 ==
-  #         coordinates[index +1][1].to_i )
-  #
-  #
-  #   consecutive = coordinates.map do |coordinate|
-  #     (coordinates[index][1].to_i + 1 ==
-  #         coordinates[index +1][1].to_i)
-  #      index += 1
-  #   end
-  # end
 end
