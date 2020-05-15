f85c3e07c882f48b7f93b8334ac72c3ec3731ceb
Sienna Kopf <princess.kopf@gmail.com>
2020-05-15 15:50:22 -0600

    Add tests for board functionality for placing ships

--- test/board_test.rb
+++ test/board_test.rb
@@ -50,4 +50,16 @@ class BoardTest < Minitest::Test
     assert_equal true, @board.valid_placement?(@cruiser, ['B1', 'C1', 'D1'])
   end
 
+  def test_placing_same_ship_obj_in_multiple_cells
+    @board.place(@cruiser, ['A1', 'A2', 'A3'])
+    cell_1 = @board.cells['A1']
+    cell_2 = @board.cells['A2']
+    cell_3 = @board.cells['A3']
+
+    assert_equal @cruiser, cell_1.ship
+    assert_equal @cruiser, cell_2.ship
+    assert_equal @cruiser, cell_3.ship
+    assert cell_3.ship == cell_2.ship
+  end
+
 end
