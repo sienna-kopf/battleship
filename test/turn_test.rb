fe2e7f82bf210c9887ee2ba41deabd0ece290427
Sienna Kopf <princess.kopf@gmail.com>
2020-05-20 13:17:17 -0600

    Add tests for new player objects and assert they have different boards and ships

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -5,6 +5,7 @@ require './lib/cell'
 require './lib/board'
 require './lib/game'
 require './lib/turn'
+require './lib/player'
 
 class TurnTest < Minitest::Test
   def setup
@@ -15,36 +16,15 @@ class TurnTest < Minitest::Test
     assert_instance_of Turn, @turn
   end
 
-  def test_it_has_attributes
-    assert_instance_of Board, @turn.computer_board
-    assert_instance_of Board, @turn.player_board
-    assert_instance_of Ship, @turn.cruiser_comp
-    assert_instance_of Ship, @turn.submarine_comp
-    assert_instance_of Ship, @turn.cruiser_play
-    assert_instance_of Ship, @turn.submarine_comp
-    assert_equal false, @turn.player_board == @turn.computer_board
-    assert_equal false, @turn.cruiser_comp == @turn.cruiser_play
-    assert_equal false, @turn.submarine_comp == @turn.submarine_play
+  def test_it_has_players
+    assert_instance_of Player, @turn.human_player
+    assert_instance_of Player, @turn.computer_player
   end
-
 
   def test_it_selects_a_new_random_cell
     random_1 = @turn.select_random_cell
     random_2 = @turn.select_random_cell
 
     assert_equal false, random_1 == random_2
-  end
-
-  def test_computer_shoots_only_new_coords
-    @turn.computer_shot
-    @turn.computer_shot
-    @turn.computer_shot
-    @turn.computer_shot
-    @turn.computer_shot
-    @turn.computer_shot
-    @turn.computer_shot
-    @turn.computer_shot
-    @turn.computer_shot
-    @turn.computer_shot
   end
 end
